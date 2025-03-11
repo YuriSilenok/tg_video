c38986a648d67a9b4a2724ceaceae36a3e7ae2f0
YuriSilenok <yuri.silenok@gmail.com>
2025-03-11 11:02:44 +0300

    отчет по проверяющим

--- admin.py
+++ admin.py
@@ -6,11 +6,12 @@ from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from common import get_due_date, get_user
-from models import TASK_STATUS, Course, Role, Task, Theme, User, UserCourse, UserRole, Video
+from models import TASK_STATUS, Course, Role, Task, Theme, User, UserCourse, UserRole, Video, ReviewRequest
 from peewee import JOIN, fn
 
 
 router = Router()
+
 
 async def get_admin_user_role(bot: Bot, user: User):
     """Проверяем наличие привилегии блогера"""
@@ -41,6 +42,7 @@ async def get_admin_user_role(bot: Bot, user: User):
         return None
 
     return user_role
+
 
 async def send_task(bot: Bot):
 
@@ -155,6 +157,8 @@ async def send_task(bot: Bot):
                 chat_id=admin.tg_id,
                 text='Нет свобоных тем или блогеров'
             )
+
+
 @router.message(Command('get_video'))
 async def get_video(message: Message):
     user = await get_user(message.bot, message.from_user.id)
@@ -170,6 +174,7 @@ async def get_video(message: Message):
         video=Video.get_by_id(video_id).file_id
     )
 
+
 @router.message(Command('add_task'))
 async def set_implementer(message: Message):
     user = await get_user(message.bot, message.from_user.id)
@@ -182,189 +187,30 @@ async def set_implementer(message: Message):
     
     await send_task(message.bot)
 
+@router.message(Command('report_reviewers'))
+async def report_reviewers(message: Message):
 
-
-@router.message(Command('show_courses'))
-async def show_courses(message: Message):
-    user = await get_user(message.bot, message.from_user.id)
-    if user is None:
-        return
-    
-    user_role = await get_admin_user_role(message.bot, user)
-    if not user_role:
-        return
-
-    # Подсчет количества тем в каждом курсе с разбивкой по статусу задачи
-    task_counts = (
-        Task.select(
-            Theme.course,
-            Task.status,
-            fn.COUNT(Task.id).alias("task_count")
-        )
-        .join(Theme)
-        .group_by(Theme.course, Task.status)
-    )
-
-    # Подсчет количества тем в каждом курсе, у которых нет задач
-    themes_without_tasks = (
-        Theme.select(
-            Theme.course,
-            fn.COUNT(Theme.id).alias("no_task_count")
-        )
-        .where(~Theme.id.in_(Task.select(Task.theme).group_by(Task.theme)))  # Темы без задач
-        .group_by(Theme.course)
-    )
-
-    # Собираем количество задач по статусу
-    task_count_dict = {}
-    for row in task_counts.dicts():
-        course_id = row["course"]
-        status = row["status"]
-        count = row["task_count"]
-        if course_id not in task_count_dict:
-            task_count_dict[course_id] = {s: 0 for s in [-2, -1, 0, 1, 2, 3]}
-        task_count_dict[course_id][status] = count
-
-    # Собираем количество тем без задач
-    no_task_count_dict = {row["course"]: row["no_task_count"] for row in themes_without_tasks.dicts()}
-
-    # Запрос списка курсов
-    # courses = Course.select().dicts()
-
-    # Формируем финальный результат
-    result = '\n\n'.join(
-        [''.join([
-            f'{course.title}\n',
-            *[f'{i}={task_count_dict.get(course.id, {i:0})[i]}'.ljust(5, ' ') for i in [-2, -1, 0, 1, 2, 3]],
-            f'no={no_task_count_dict.get(course.id, 0)}',
-        ]) for course in Course.select()]
-    )
-    # for course in courses:
-    #     course_id = course["id"]
-    #     course["task_counts"] = task_count_dict.get(course_id, {s: 0 for s in [-2, -1, 0, 1, 2, 3]})
-    #     course["no_task_count"] = no_task_count_dict.get(course_id, 0)
-    #     results.append(course)
-
-    # Вывод результата
-    print(result)
-
-
-
-    await message.answer(
-        text=result
-    )
-    
-@router.message(Command('show_themes'))
-async def show_themes(message: Message):
-    user = await get_user(message.bot, message.from_user.id)
-    if user is None:
-        return
-    
-    user_role = await get_admin_user_role(message.bot, user)
-    if not user_role:
-        return
-
-    course_title = message.text.split(maxsplit=1)[1].strip()
-    course = Course.get_or_none(
-        title=course_title
-    )
-    if course is None:
-        await message.answer(
-            text='Курс с таким названием не найден'
-        )
-        return
-    
-    query = (
-        Theme
+    reviewers = (
+        User
         .select(
-            Theme.title,
-            fn.MAX(Task.status).alias('status_max')
+            User.comment.alias('fio'),
+            User.reviewer_score.alias('score'),
+            fn.COUNT(ReviewRequest).alias('count')
         )
-        .join(
-            Task,
-            JOIN.LEFT_OUTER,
-        )
+        .join(UserRole)
+        .join(Role)
+        .join(ReviewRequest, on=(ReviewRequest.reviewer_id==User.id))
         .where(
-            Theme.course_id==course.id
+            (Role.name == 'Проверяющий') &
+            (ReviewRequest.status == 1) # Видео проверено
         )
-        .group_by(
-            Theme.id
-        )
+        .group_by(User)
     )
+    result = 'Отчет о проверяющих\n\n'
+    result += '\n'.join([
+        f"{i['count']} {i['score']} {i['fio']}" for i in reviewers.dicts()
+    ])
 
-    result = (
-        '\n\n'.join([
-            f'{i.title}\n{"Не выдавалась" if i.status_max is None else TASK_STATUS[i.status_max]}' for i in query
-        ])
-    )
-
-    print(query.sql()[0])
-
-    # for row in query.dicts():
-    #     print(row)
-    
-    # # Подсчет количества тем в каждом курсе с разбивкой по статусу задачи
-    # task_counts = (
-    #     Task.select(
-    #         Theme.id.alias("theme_id"),
-    #         Task.status,
-    #         fn.COUNT(Task.id).alias("task_count")
-    #     )
-    #     .join(Theme)
-    #     .where(Theme.course_id == course.id)
-    #     .group_by(Theme.id, Task.status)
-    # )
-
-    # # Подсчет количества тем в каждом курсе, у которых нет задач
-    # themes_without_tasks = (
-    #     Theme.select(
-    #         Theme.id.alias("theme_id"),
-    #         fn.COUNT(Theme.id).alias("no_task_count")
-    #     )
-    #     .where(
-    #         (Theme.course_id == course.id) &
-    #         (~Theme.id.in_(
-    #             Task
-    #             .select(Task.theme)
-    #             .group_by(Task.theme))))
-    #     .group_by(Theme.id)
-    # )
-
-    # # Собираем количество задач по статусу
-    # task_count_dict = {}
-    # for row in task_counts.dicts():
-    #     theme_id = row["theme_id"]
-    #     status = row["status"]
-    #     count = row["task_count"]
-    #     if theme_id not in task_count_dict:
-    #         task_count_dict[theme_id] = {s: 0 for s in [-2, -1, 0, 1, 2, 3]}
-    #     task_count_dict[theme_id][status] = count
-
-    # # Собираем количество тем без задач
-    # no_task_count_dict = {row["theme_id"]: row["no_task_count"] for row in themes_without_tasks.dicts()}
-
-    # # Запрос списка курсов
-    # # courses = Course.select().dicts()
-
-    # # Формируем финальный результат
-    # result = '\n\n'.join(
-    #     [''.join([
-    #         f'{theme.title}\n',
-    #         *[f'{i}={task_count_dict.get(theme.id, {i:0})[i]}'.ljust(5, ' ') for i in [-2, -1, 0, 1, 2, 3]],
-    #         f'no={no_task_count_dict.get(theme.id, 0)}',
-            
-    #     ]) for theme in Theme.select().where(Theme.course_id==course.id)]
-    # )
-    # # for course in courses:
-    # #     course_id = course["id"]
-    # #     course["task_counts"] = task_count_dict.get(course_id, {s: 0 for s in [-2, -1, 0, 1, 2, 3]})
-    # #     course["no_task_count"] = no_task_count_dict.get(course_id, 0)
-    # #     results.append(course)
-
-    # # Вывод результата
-    
-    print(result)
     await message.answer(
         text=result
     )
-    
