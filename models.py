dc70145d81f2d66ada3e5ac928bf4ac3235ce434
YuriSilenok <yuri.silenok@gmail.com>
2025-04-01 23:21:24 +0300

    устал

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import math
-from typing import List, Tuple
-from peewee import Model, SqliteDatabase, JOIN, fn, Cast, BooleanField, FloatField, CharField, IntegerField, ForeignKeyField, DateTimeField, Value
+from typing import Dict, List, Tuple
+from peewee import Model, SqliteDatabase, JOIN, fn, Case, BooleanField, FloatField, CharField, IntegerField, ForeignKeyField, DateTimeField, Value
 
 
 db = SqliteDatabase('sqlite.db')
@@ -52,24 +52,143 @@ class User(Table):
         return f'<a href="https://t.me/{self.username}">{surname}</a>'
 
 
-
-    @classmethod
-    def link_alias(cls):
-        # Извлекаем первое слово из comment или используем "Аноним", если comment пустой
-        surname = fn.COALESCE(
-            fn.SUBSTR(
-                cls.comment,
-                1,
-                fn.INSTR(cls.comment, ' ') - 1  # Находим позицию первого пробела
-            ),
-            Value("Аноним")
+    def get_avg_duration(self):
+        """Получить общую продолжительность всех проверенных видео в секундах"""
+        return (
+            ReviewRequest
+            .select(fn.SUM(Video.duration))
+            .join(Video)
+            .where(
+                (ReviewRequest.reviewer_id == self.id) &
+                (ReviewRequest.status == 1)
+            )
+            .scalar()
         )
-        # Формируем строку с использованием fn.printf
-        result = fn.printf(
-            Value('<a href="https://t.me/%s">%s</a>'), cls.username, surname
-        )
-        return result.alias('link')
 
+
+    def update_reviewer_score(self):
+        """Пересчитать баллы проверяющего"""
+
+        self.reviewer_score = self.get_avg_duration() / 1200
+        self.save()
+
+
+    def get_reviewer_rating_from_score(self):
+        '''Получить рейтинг проверяющего по оценками'''
+        
+        min_score, max_score = Review.get_minmax_score()
+        delta = max_score - min_score
+        video_avg_scores = Review.get_avg_scores()
+        
+        data = [
+            abs(video_avg_scores[row['video']] - row['score']) / delta for row in
+            Review
+            .select(
+                ReviewRequest.video,
+                Review.score,
+            )
+            .join(ReviewRequest)
+            .where(ReviewRequest.reviewer == self.id)
+            .dicts()
+        ]
+
+        return sum(data) / len(data)
+
+
+    def get_reviewer_rating_from_over(self):
+        """Получить рейтинг проверяющего по просрочкам"""
+
+
+    def update_reviewers_rating(self):
+        '''Обновление рейтинга проверяющего'''
+    
+        rating_from_score = self.get_reviewer_rating_from_score()
+
+
+        """Расчет пропусков проверок"""
+
+        if len(reviewer_overs) == 0:
+            return None
+        max_reviewer = max(reviewer_overs, key=reviewer_overs.get)
+        max_reviewer_over = reviewer_overs[max_reviewer]
+
+        rating = {}
+        for reviewer in list(set(score_over.keys()) | set(reviewer_overs.keys())):
+            rating[reviewer] = (
+                score_over.get(reviewer, 0) + 
+                (reviewer_overs.get(reviewer, 0)/max_reviewer_over) ** 2
+            )
+
+        max_hours = max([row['hours'] for row in 
+            ReviewRequest
+            .select(
+                fn.AVG(
+                    fn.ROUND(
+                        (fn.julianday(Review.at_created) - fn.julianday(ReviewRequest.at_created)) * 24,
+                        2
+                    )
+                ).alias('hours')
+            )
+            .join(Review)
+            .group_by(
+                ReviewRequest.reviewer
+            ).dicts()
+        ])
+
+        query: List[Review] = (
+            Review
+            .select(
+                    ReviewRequest.reviewer.alias('reviewer'),
+                    fn.AVG(
+                        fn.ROUND(
+                            (fn.julianday(Review.at_created) - fn.julianday(ReviewRequest.at_created)) * 24,
+                            2
+                        )
+                    ).alias('hours')
+            )
+            .join(ReviewRequest)
+            .group_by(ReviewRequest.reviewer)
+        )
+        for row in query.dicts():
+            rating[row['reviewer']] = (
+                rating.get(row['reviewer'], 0) + 
+                (row['hours']/max_hours) ** 2
+            )
+
+
+        for reviewer in rating:
+            user: User = User.get_by_id(reviewer)
+            user.reviewer_rating = math.sqrt(rating[reviewer])
+            user.save()
+
+
+    def update_bloger_score(self):
+
+        self.bloger_rating = bloger_avg_tsk_score
+        result = f'Ваш рейтинг: {bloger_avg_tsk_score}\n'
+        
+        bloger_score = 0
+        tasks = (Task
+            .select()
+            .where(
+                (Task.implementer == bloger) &
+                (Task.status.not_in([0,1]))
+            )
+        )
+        result += f'Видео которые Вы записали были оценены:\n\n'
+        i = 0
+        for task in tasks:
+            k = 1.05**i
+            complexity = task.theme.complexity
+            score = task.score * k * complexity
+            bloger_score += score
+            i+=1
+            result +=  f'{task.theme.title}\ns*k*c={round(task.score, 4)}*{round(k, 4)}*{complexity}={score}\n\n'
+        result += f's = Оценка за видео, k=за стаж, с=за объем материала\n'
+        bloger.bloger_score = round(bloger_score, 2)
+        bloger.save()
+        result += f'ИТОГО БАЛЛОВ: {bloger.bloger_score}'
+        return result
 
 
 class Role(Table):
@@ -116,6 +235,54 @@ class Task(Table):
     # 1 - кнопка о продлении отправлена
     extension = IntegerField(default=0)
 
+    @staticmethod
+    def get_avg_duration():
+        """Получить среднюю продолжительность выполнения задачи для каждого блогера"""
+        return {row['bloger']: (row['avg_hours'] if row['avg_hours'] else 0) for row in
+            Task
+            .select(
+                fn.AVG(
+                        Case(
+                            None,
+                            [(
+                                Task.status==0, 
+                                (fn.julianday(datetime.now()) - fn.julianday(Task.at_created)) * 24
+                            )],
+                            (fn.julianday(Video.at_created) - fn.julianday(Task.at_created)) * 24
+                        ) / Theme.complexity
+                ).alias('avg_hours'),
+                Task.implementer.alias('bloger')
+            )
+            .join(Theme)
+            .join(Video, JOIN.LEFT_OUTER, on=(Video.task==Task.id))
+            .where(
+                (Task.status != -1)
+            )
+            .group_by(
+                Task.implementer
+            )
+            .dicts()
+        }
+
+
+    @staticmethod
+    def get_avg_score():
+        """Получить средние оценки по задачам для каждого блогера"""
+        return {row['bloger']: row['score'] for row in
+            Task 
+            .select(
+                fn.AVG(Task.score).alias('score'),
+                Task.implementer.alias('bloger')
+            )
+            .where(
+                (Task.status.not_in([0,1]))
+            )
+            .group_by(
+                (Task.implementer)
+            )
+            .dicts()
+        }
+
 
 class Video(Table):
     """Видео которое было прислано на оценку по задаче"""
@@ -134,12 +301,72 @@ class ReviewRequest(Table):
     due_date = DateTimeField()
 
 
+    @staticmethod
+    def get_overs():
+        """Получить список просрочек каждого проверяющего"""
+
+        sql_query = """
+        select rr1.reviewer_id, count(rr2.video_id)
+        from (
+            select rr.reviewer_id
+            from reviewrequest as rr
+            group by rr.reviewer_id
+        ) as rr1
+        left join reviewrequest as rr2 on rr2.reviewer_id = rr1.reviewer_id and rr2.status = -1
+        group by rr1.reviewer_id
+        """
+
+        return {i['reviewer_id']: i['video_id'] for i in
+            Table.raw(sql_query).dicts()
+        }
+
 class Review(Table):
     """Результат проверки видео"""
     review_request = ForeignKeyField(ReviewRequest, backref='reviews', **CASCADE)
     score = FloatField()
     comment = CharField()
     at_created = DateTimeField(default=datetime.now)
+
+
+    @staticmethod
+    def get_avg_scores() -> Dict[int, int]:
+        '''Плучить среднюю оценку для каждого видео'''
+
+        return { row['video']: row['avg'] for row in
+            Review
+            .select(
+                fn.AVG(Review.score).alias('avg'),
+                ReviewRequest.video.alias('video'),
+            )
+            .join(ReviewRequest)
+            .group_by(ReviewRequest.video)
+            .dicts()
+        } 
+
+
+    @staticmethod
+    def get_minmax_score() -> Tuple[int, int]:
+        """Получить минимальное и максимальное отклонения оценки от средней оценки"""
+
+        data = [
+            (
+                min(abs(row['avg']-row['min']), abs(row['avg']-row['max'])), 
+                max(abs(row['avg']-row['min']), abs(row['avg']-row['max']))
+            ) for row in
+            Review
+            .select(
+                fn.MIN(Review.score).alias('min'),
+                fn.MAX(Review.score).alias('max'),
+                fn.AVG(Review.score).alias('avg'),
+            )
+            .join(ReviewRequest)
+            .group_by(ReviewRequest.video)
+            .dicts()
+        ]
+        return (
+            min(data, key=lambda i: i[0])[0],
+            max(data, key=lambda i: i[1])[1]
+        )
 
 
 class Poll(Table):
@@ -154,209 +381,6 @@ class Var(Table):
     value = CharField(null=True)
 
 
-def get_videos_by_request_review(user: User) -> List[User]:
-    """Получить все видео, требующие этого проверяющего"""
-
-    query = (
-        Video
-        .select(Video)
-        .join(Task, on=(Video.task == Task.id))
-        .join(ReviewRequest, JOIN.LEFT_OUTER, on=(ReviewRequest.video == Video.id))
-        .where(
-            (Task.status == 1) &  # Задача в статусе 1
-            (Video.id.not_in(
-                Video
-                .select(Video.id)
-                .join(ReviewRequest)
-                .where(ReviewRequest.reviewer_id == user.id)  # Исключаем видео, которые я проверял
-            ))
-        )
-        .group_by(Video.id)
-        .having(fn.COUNT(ReviewRequest.id) < 5)  # У видео должно быть < 5 запросов на проверку
-    )
-
-    return list(query.execute())
-
-
-def update_bloger_score_and_rating(bloger: User):
-
-    bloger_rating = (Task
-        .select(fn.AVG(Task.score))
-        .where(
-            (Task.implementer == bloger) &
-            (Task.status.not_in([0,1]))
-        )
-        .scalar()
-    )
-
-    bloger.bloger_rating = bloger_rating if bloger_rating else 0.8
-    result = f'Ваш рейтинг: {bloger_rating}\n'
-    
-    bloger_score = 0
-    tasks = (Task
-        .select()
-        .where(
-            (Task.implementer == bloger) &
-            (Task.status.not_in([0,1]))
-        )
-    )
-    result += f'Видео которые Вы записали были оценены:\n\n'
-    i = 0
-    for task in tasks:
-        k = 1.05**i
-        complexity = task.theme.complexity
-        score = task.score * k * complexity
-        bloger_score += score
-        i+=1
-        result +=  f'{task.theme.title}\ns*k*c={round(task.score, 4)}*{round(k, 4)}*{complexity}={score}\n\n'
-    result += f's = Оценка за видео, k=за стаж, с=за объем материала\n'
-    bloger.bloger_score = round(bloger_score, 2)
-    bloger.save()
-    result += f'ИТОГО БАЛЛОВ: {bloger.bloger_score}'
-    return result
-
-
-def get_max_score_over():
-    max_score_over = (
-        Review
-        .select(
-            (fn.MAX(Review.score)-fn.MIN(Review.score)).alias('score'),
-        )
-        .join(ReviewRequest)
-        .group_by(ReviewRequest.video)
-    )
-    return max(max_score_over, key=lambda i: i.score).score
-
-
-def update_reviewers_rating():
-    '''Обновление рейтинга проверяющего'''
-  
-    """Расчет завышения оценки"""
-    video_avg_scores = {i['video']:i['score'] for i in
-        Review
-        .select(
-            (fn.AVG(Review.score)).alias('score'),
-            ReviewRequest.video,
-        )
-        .join(ReviewRequest)
-        .where(ReviewRequest.status==1)
-        .group_by(ReviewRequest.video)
-        .dicts()
-    }
-
-    reviewer_scores = (
-        ReviewRequest
-        .select(
-            ReviewRequest.video,
-            ReviewRequest.reviewer,
-            Review.score,
-        )
-        .join(Review)
-    )
-
-    score_over = {}
-
-    for row in reviewer_scores.dicts():
-        score_over[row['reviewer']] = (
-            score_over.get(row['reviewer'], []) 
-            + [abs(row['score'] - video_avg_scores[row['video']])]
-        )
-
-    for reviewer in score_over:
-        scores = score_over[reviewer]
-        avg_score = (sum(scores) / len(scores)) ** 2
-        score_over[reviewer] = avg_score
-
-
-    """Расчет пропусков проверок"""
-    reviewer_overs = {i['reviewer']: i['over'] for i in
-        ReviewRequest
-        .select(
-            ReviewRequest.reviewer,
-            fn.COUNT(ReviewRequest.reviewer).alias('over')
-        )
-        .where(
-            (ReviewRequest.status==-1)
-        )
-        .group_by(ReviewRequest.reviewer)
-        .dicts()
-    }
-    if len(reviewer_overs) == 0:
-        return None
-    max_reviewer = max(reviewer_overs, key=reviewer_overs.get)
-    max_reviewer_over = reviewer_overs[max_reviewer]
-
-    rating = {}
-    for reviewer in list(set(score_over.keys()) | set(reviewer_overs.keys())):
-        rating[reviewer] = (
-            score_over.get(reviewer, 0) + 
-            (reviewer_overs.get(reviewer, 0)/max_reviewer_over) ** 2
-        )
-
-    max_hours = max([row['hours'] for row in 
-        ReviewRequest
-        .select(
-            fn.AVG(
-                fn.ROUND(
-                    (fn.julianday(Review.at_created) - fn.julianday(ReviewRequest.at_created)) * 24,
-                    2
-                )
-            ).alias('hours')
-        )
-        .join(Review)
-        .group_by(
-            ReviewRequest.reviewer
-        ).dicts()
-    ])
-
-    query: List[Review] = (
-        Review
-        .select(
-                ReviewRequest.reviewer.alias('reviewer'),
-                fn.AVG(
-                    fn.ROUND(
-                        (fn.julianday(Review.at_created) - fn.julianday(ReviewRequest.at_created)) * 24,
-                        2
-                    )
-                ).alias('hours')
-        )
-        .join(ReviewRequest)
-        .group_by(ReviewRequest.reviewer)
-    )
-    for row in query.dicts():
-        rating[row['reviewer']] = (
-            rating.get(row['reviewer'], 0) + 
-            (row['hours']/max_hours) ** 2
-        )
-
-
-    for reviewer in rating:
-        user: User = User.get_by_id(reviewer)
-        user.reviewer_rating = math.sqrt(rating[reviewer])
-        user.save()
-
-
-def update_reviewer_score(reviewer: User):
-
-    review_requests = (
-        ReviewRequest
-        .select()
-        .join(Video)
-        .where(
-            (ReviewRequest.reviewer_id == reviewer.id) &
-            (ReviewRequest.status == 1)
-        )
-    )
-    
-    reviewer_score = 0
-    for review_request in review_requests:
-        reviewer_score += review_request.video.duration / 1200
-    
-    reviewer.reviewer_score = round(reviewer_score, 2)
-    reviewer.save()
-    return reviewer.reviewer_score
-
-
 if __name__ == '__main__':
     db.create_tables([
         User, Role, UserRole,
@@ -365,16 +389,5 @@ if __name__ == '__main__':
         UserCourse, Poll, Var
     ])        
 
-    for user in User.select():
-        update_reviewer_score(user)
-        update_bloger_score_and_rating(user)
-    update_reviewers_rating()
-
-    for theme in Theme.select():
-        title = theme.course.title
-
-        if theme.title.startswith(title):
-            if theme.title.startswith(f'{title} - '):
-                title = f'{title} - '
-            theme.title = theme.title.replace(title, '').strip()
-            theme.save()
+    for row in ReviewRequest.get_overs().items():
+        print(*row)
