db3a03e1eaa5ed1fe69d3a68458e82add2e83335
Dekorlol <evgengolovin561@gmail.com>
2025-04-05 14:46:20 +0300

    Исправлен текст приветствия

--- user.py
+++ user.py
@@ -118,18 +118,18 @@ async def start(message: Message):
 
     user = User.get(tg_id=message.from_user.id)
 
-    await message.answer(
-        "Здравствуйте, Вы запустили бота который выдает темы для записи видео."
-    )
+    user_text = "Здравствуйте, Вы запустили бота который выдает темы для записи видео."
 
     if user.comment is None:
         await message.answer(
-            text = (
-                "Представьтесь, укажите свои ФИО отправив команду в следующем формате "
+            text = (user_text +
+                " Представьтесь, укажите свои ФИО отправив команду в следующем формате "
                 "<b>/set_fio Иванов Иван Иванович</b>"
             ),
             parse_mode='HTML',
         )
+    else:
+        await message.answer(user_text)
 
 
 @router.message(Command('report'), IsUser())
