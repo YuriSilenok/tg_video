c8e54fdda418a67785ae6a52762b4414a6d665e4
Syshacker20 <kirildekster@gmail.com>
2025-04-07 17:39:38 +0300

    Добавление кнопок для администратора

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 from typing import List
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message, BotCommand, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, BotCommand, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup
 
 
 from common import error_handler, send_task, send_message_admins
@@ -70,7 +70,8 @@ async def start(message: Message):
     elif user.username != message.from_user.username:
         user.username = message.from_user.username
         user.save()
-
+    
+    
 
     commands = [
         BotCommand(
@@ -95,6 +96,17 @@ async def start(message: Message):
     await message.bot.set_my_commands(
         commands=commands   
     )
+    reply_markup = None
+    if UserRole.get_or_none(user=user, role=IsAdmin.role):
+      
+        keyboard =[
+            [KeyboardButton(text="/report_reviewers")],
+            [KeyboardButton(text="/report_blogers")],
+            [KeyboardButton(text="/report_tasks")]
+        ]
+        reply_markup = ReplyKeyboardMarkup(keyboard=keyboard)
+
+    
 
     await message.answer(
         text = (
@@ -102,7 +114,8 @@ async def start(message: Message):
             "Представьтесь, укажите свои ФИО отправив команду в следующем формате "
             "<b>/set_fio Иванов Иван Иванович</b>"
         ),
-        parse_mode='HTML',
+        parse_mode='HTML', 
+        reply_markup=reply_markup
     )
 
 
