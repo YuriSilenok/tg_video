8cf7e38ffff47762c542ffc4e05163e0e483acd4
OrlovMatvey <deceptor27@mail.ru>
2025-07-08 19:17:54 +0300

    Попытался разделить название курса и теги. Снова.

--- channel.py
+++ channel.py
@@ -35,7 +35,9 @@ async def send_video(bot: Bot, video_obj: Video = None):
 
     task = video_obj.task
     theme = task.theme
-    course_title = theme.course.title
+    course_title = theme.course
+    tegi = course_title.tegi
+
     ch = [
         ("-", ""),
         (".", ""),
@@ -45,19 +47,14 @@ async def send_video(bot: Bot, video_obj: Video = None):
     ]
     for ch1, ch2 in ch:
         course_title = course_title.replace(ch1, ch2)
+        tegi = course_title.replace(ch1, ch2)
 
-    course_no_name = course_title.split()
-
-    course_title = " #".join(course_title.split())
-
-    del course_no_name[0]
-
-    course_no_name = " #".join(course_no_name.split())
+    tegi = " #".join(course_title.split())
 
     caption = (
-        f"Курс: {course_title.rsplit(sep=' #', maxsplit=1)[0]}\n"
+        f"Курс: {course_title.title}"
         f'Тема: <a href="{theme.url}">{theme.title}</a>'
-        f'Теги: #{course_no_name}\n'
+        f'Теги: #{tegi}\n'
     )
     message = await bot.send_video(
         chat_id=TG_CHANEL_ID,
